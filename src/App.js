48d98dc149cbbdcf8d1d4b46724e18088f9a8ca9
Benji Thompson <thompson.benji@gmail.com>
2021-07-12 18:03:01 -0400

    basic color scheme selected, heading added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div className="mt-4 container text-center">
-        <h1>BENJI THOMPSON</h1>
+      <div className="header">
+        <div className="container text-center">
+          <h1 className="pt-4 pb-4">BENJI THOMPSON</h1>
+        </div>
       </div>
     )
   }
