bc443b0571d316499cbac6315d5719441a6670d1
Benji Thompson <thompson.benji@gmail.com>
2021-07-13 17:14:31 -0400

    additional update to About section

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default class About extends Component {
                 {/* <div className="pt-2 container text-center">
                     <h4>ABOUT</h4>
                 </div> */}
-                <div className="pt-4 row justify-content-between">
+                <div className="pt-4 row justify-content-center">
                     <div className="col-6 ms-4">
                         <h5>Background:</h5>
                         <p>Since 2015, I've been working in tech startups leading in a variety of roles alongside customer support, product, and engineering teams. In 2018, while working in a non-technical role, I built an onboarding & support chatbot which would allow our company to scale more efficiently in a new market. This was a valuable experience for me because it revealed that in addition to my skillsets as a capable project & operations leader, I learned that I am, especially, a creative engineer. I love building products and experiences that help facilitate improvements to sustained quality of life on our planet (and maybe one day, other planets also).</p>
@@ -15,8 +15,8 @@ export default class About extends Component {
                     <div className="col-5 me-4">
                         <h5>Technology:</h5>
                         <ul>
-                            <li>HTML, CSS, Javascript, SQL</li>
                             <li>MongoDB, Express.js, React, Node.js (MERN stack)</li>
+                            <li>HTML, CSS, Javascript</li>
                             <li>I also have familiarity with Python/Flask and Postgres and would be interested in learning other languages/frameworks for the right role and team.</li>
                         </ul>
                     </div>
