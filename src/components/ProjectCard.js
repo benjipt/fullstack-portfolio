c0060f0ff84438ce09a999271871b0e05c699930
Benji Thompson <thompson.benji@gmail.com>
2021-07-13 20:19:41 -0400

    removed images from cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react'
 
-// const cardStyle = {
-//     width: "18rem"
-// }
-
 // const imgStyle = {
-//     width: "16rem",
+//     display: "block",
+//     width: "100%",
 //     height: "auto"
 // }
 
 export default class ProjectCard extends Component {
     render() {
         return (
-            <div className="col-3">
+            <div className="col-5 pb-3">
                 <div className="card">
-                    <img src={ this.props.project.img } className="card-img-top img-fluid" alt={ this.props.project.name } />
+                    {/* <img style={imgStyle} src="" className="card-img-top" alt={ this.props.project.name } /> */}
                     <div className="card-body">
                         <h5 className="card-title">{ this.props.project.name }</h5>
                         <p className="card-text">{ this.props.project.description }</p>
-                        <a className="btn btn-secondary" href={ this.props.project.github }>Github Repo</a>
-                        <a className="btn btn-primary" href={ this.props.project.site }>Launch</a>
+                        <a className="btn btn-sm btn-secondary me-2" href={ this.props.project.github }>Github Repo</a>
+                        <a className="btn btn-sm btn-primary" href={ this.props.project.site }>Launch</a>
                     </div>
                 </div>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,18 +8,18 @@ export default class Projects extends Component {
         this.state = {
             projects: [
                 {
-                    img: 'src/images/simon.png',
+                    img: '../images/journals.jpg',
+                    name: 'Journals',
+                    description: 'A full-stack CRUD React app that allows users to write and store journal entries. An autocomplete search input is provided to help users search and select for specific journal entries based on keywords.',
+                    github: 'https://github.com/benjipt/journal-app-frontend',
+                    site: 'https://cryptolog-frontend.herokuapp.com/',
+                    id: 1
+                }, {
+                    img: '../images/simon-card-img.jpg',
                     name: 'Simon',
                     description: 'A digital recreation of Simon, the memory game from Hasbro, with some modern updates. Sounds were created with Garageband.',
                     github: 'https://github.com/benjipt/simon-game',
                     site: 'https://wonderful-brahmagupta-3ad87a.netlify.app/',
-                    id: 1
-                }, {
-                    img: 'src/images/microconomy.png',
-                    name: 'Microconomy',
-                    description: 'Privacy focused referral networks. Full-stack CRUD app using Express JS, deployed on Heroku.',
-                    github: 'https://github.com/benjipt/microeconomy',
-                    site: 'http://microeconomy.herokuapp.com/',
                     id: 2
                 }, {
                     img: 'src/images/cryptolog.png',
@@ -29,11 +29,11 @@ export default class Projects extends Component {
                     site: 'https://cryptolog-frontend.herokuapp.com/',
                     id: 3
                 }, {
-                    img: '../images/journals.jpg',
-                    name: 'Journals',
-                    description: 'A full-stack CRUD React app that allows users to write and store journal entries. An autocomplete search input is provided to help users search and select for specific journal entries based on keywords.',
-                    github: 'https://github.com/benjipt/journal-app-frontend',
-                    site: 'https://cryptolog-frontend.herokuapp.com/',
+                    img: 'src/images/microconomy.png',
+                    name: 'Microconomy',
+                    description: 'Privacy focused referral networks. Full-stack CRUD app using Express JS, deployed on Heroku.',
+                    github: 'https://github.com/benjipt/microeconomy',
+                    site: 'http://microeconomy.herokuapp.com/',
                     id: 4
                 }
             ]
@@ -44,17 +44,14 @@ export default class Projects extends Component {
 
     render() {
         return (
-            <div className="bottom">
-                <div className="pt-2 container text-center">
-                    <h4>PROJECTS</h4>
-                    <div className="row">
-                        { this.state.projects.map((project) => {
-                            return (
-                                <ProjectCard project={project}
-                                             key={project.id} />
-                            )
-                        }) }
-                    </div>
+            <div className="bottom text-center">
+                <div className="row justify-content-center pt-4">
+                    { this.state.projects.map((project) => {
+                        return (
+                            <ProjectCard project={project}
+                                            key={project.id} />
+                        )
+                    }) }
                 </div>
             </div>
         )
