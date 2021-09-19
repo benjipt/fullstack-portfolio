03cedb99c2620dd33847c398f8af14f84f15c00f
Benji Thompson <thompson.benji@gmail.com>
2021-09-19 15:42:13 -0400

    refactored Header from class to function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import headshot from '../images/IMG_9582.jpg'
 
 const imgStyle = {
@@ -6,28 +6,26 @@ const imgStyle = {
     height: 'auto'
 }
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div className="header text-center">
-                <nav className="navbar navbar-dark bg-custom ps-4">
-                    <form className="justify-content-start">
-                        <button className="btn btn-sm btn-outline-dark me-3"
-                                onClick={ this.props.showAbout }>ABOUT</button>
-                        <button className="btn btn-sm btn-outline-dark"
-                                onClick={ this.props.showProjects }>PROJECTS</button>
-                    </form>
-                    <div className="d-flex flex-row-reverse">
-                        <a className="nav-link" href="https://github.com/benjipt" target="_blank" rel="noreferrer">Github</a>
-                        <a className="nav-link" href="https://www.linkedin.com/in/benjithompson/" target="_blank" rel="noreferrer">Linkedin</a>
-                    </div>
-                </nav>
-                <h1 className="pt-4">BENJI THOMPSON</h1>
-                <img src={headshot} className="pt-4 rounded" style={imgStyle} alt="headshot Benji Thompson" />
-                <div className="pt-4 pb-1 container">
-                    <p>Full-stack web developer and React engineer based in Brooklyn, NY</p>
+export default function Header({ showAbout, showProjects }) {
+    return (
+        <div className="header text-center">
+            <nav className="navbar navbar-dark bg-custom ps-4">
+                <form className="justify-content-start">
+                    <button className="btn btn-sm btn-outline-dark me-3"
+                            onClick={ showAbout }>ABOUT</button>
+                    <button className="btn btn-sm btn-outline-dark"
+                            onClick={ showProjects }>PROJECTS</button>
+                </form>
+                <div className="d-flex flex-row-reverse">
+                    <a className="nav-link" href="https://github.com/benjipt" target="_blank" rel="noreferrer">Github</a>
+                    <a className="nav-link" href="https://www.linkedin.com/in/benjithompson/" target="_blank" rel="noreferrer">Linkedin</a>
                 </div>
+            </nav>
+            <h1 className="pt-4">BENJI THOMPSON</h1>
+            <img src={headshot} className="pt-4 rounded" style={imgStyle} alt="headshot Benji Thompson" />
+            <div className="pt-4 pb-1 container">
+                <p>Full-stack web developer and React engineer based in Brooklyn, NY</p>
             </div>
-        )
-    }
+        </div>
+    )
 }
