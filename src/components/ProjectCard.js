5ca66672c5dc6b7b732a9098b85d71854197e38d
Benji Thompson <thompson.benji@gmail.com>
2021-09-19 15:44:49 -0400

    refactored ProjectCard from class to function

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class ProjectCard extends Component {
-    render() {
-        return (
-            <div className="col-md pb-3">
-                <div className="card">
-                    <img src={this.props.project.img} className="card-img-top" alt="card img" />
-                    <div className="card-body">
-                        <h5 className="card-title">{ this.props.project.name }</h5>
-                        <p className="card-text">{ this.props.project.description }</p>
-                        <a className="btn btn-sm btn-secondary me-2 mb-1" href={ this.props.project.github }>Github Repo</a>
-                        { this.props.project.githubAPI &&
-                        <a className="btn btn-sm btn-secondary me-2 mb-1" href={ this.props.project.githubAPI }>API Github Repo</a> }
-                        <a className="btn btn-sm btn-primary" href={ this.props.project.site }>Launch</a>
-                    </div>
+export default function ProjectCard({ project }) {
+
+    const { img, name, description, github, githubAPI, site } = project
+
+    return (
+        <div className="col-md pb-3">
+            <div className="card">
+                <img src={ img } className="card-img-top" alt="card img" />
+                <div className="card-body">
+                    <h5 className="card-title">{ name }</h5>
+                    <p className="card-text">{ description }</p>
+                    <a className="btn btn-sm btn-secondary me-2 mb-1" href={ github }>Github Repo</a>
+                    { githubAPI &&
+                    <a className="btn btn-sm btn-secondary me-2 mb-1" href={ githubAPI }>API Github Repo</a> }
+                    <a className="btn btn-sm btn-primary" href={ site }>Launch</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
