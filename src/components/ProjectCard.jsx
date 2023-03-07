c61933cfbd98e24bc4e05429a03122384f80a532
Benji Thompson <thompson.benji@gmail.com>
2023-03-07 17:07:56 -0500

    feat [projects]: add pricing controller to projects and reintroduce microconomy

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export default function ProjectCard({ project }) {
-  const { img, name, description, github, githubAPI, site, deprecated } =
-    project;
+  const { img, name, description, github, githubAPI, site } = project;
 
   return (
     <div className='col-sm pt-4'>
@@ -18,22 +17,35 @@ export default function ProjectCard({ project }) {
             }}>
             <p className='card-text'>{description}</p>
           </div>
-          <a className='btn btn-sm btn-custom-secondary-card me-2' href={github} target='_blank'
-          rel='noreferrer'>
-            Github Repo
-          </a>
-          {githubAPI && (
-            <a className='btn btn-sm btn-custom-secondary-card me-2' href={githubAPI} target='_blank'
-            rel='noreferrer'>
-              API Github Repo
-            </a>
-          )}
-          {!deprecated && (
-            <a className='btn btn-sm btn-custom-card' href={site} target='_blank'
-            rel='noreferrer'>
-              Launch
-            </a>
-          )}
+          <div className='mt-1' style={{ height: '30px' }}>
+            {github && (
+              <a
+                className='btn btn-sm btn-custom-secondary-card me-2'
+                href={github}
+                target='_blank'
+                rel='noreferrer'>
+                Github Repo
+              </a>
+            )}
+            {githubAPI && (
+              <a
+                className='btn btn-sm btn-custom-secondary-card me-2'
+                href={githubAPI}
+                target='_blank'
+                rel='noreferrer'>
+                API Github Repo
+              </a>
+            )}
+            {site && (
+              <a
+                className='btn btn-sm btn-custom-card'
+                href={site}
+                target='_blank'
+                rel='noreferrer'>
+                Launch
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,11 +4,19 @@ import ProjectCard from './ProjectCard';
 import simonCardImg from '../images/simon-card-img.jpg';
 import journalsCardImg from '../images/journals-card-img.jpg';
 import cryptologCardImg from '../images/cryptolog-card-img.jpg';
-// import microconomyCardImg from '../images/microconomy-card-img.jpg'
+import microconomyCardImg from '../images/microconomy-card-img.jpg'
 import jobcoinCardImg from '../images/jobcoin-card-img.jpg';
+import confidentialCardImG from '../images/confidential-card-img.jpg';
 
 export default function Projects() {
   const projects = [
+    {
+      img: confidentialCardImG,
+      name: 'Pricing Controller',
+      description:
+        'Independently developed and deployed this React microservice for Odeko, which streamlines the pricing management process for e-commerce catalog items, replacing manual work with automated features. This saves the company significant amounts of time and licensing costs. Built with: Typescript, React, and Redux, on top of a federated graphql architecture using Apollo and urql.',
+      site: 'https://odeko.com',
+    },
     {
       img: jobcoinCardImg,
       name: 'Jobcoin',
@@ -16,8 +24,6 @@ export default function Projects() {
         'Sign-on page and UI for a fake cryptocurrency wallet - users can send coins to any address and sign-in via any address, data-visualization/chart renders balance changes in wallet over time. Take-home project for a crypto exchange, data fetched and shaped from exchange API for wallet balance rendering. Only certain addresses will have coins in wallet, try: Jilly or Benji for demo purposes.',
       github: 'https://github.com/benjipt/jobcoin-ui',
       site: 'https://jobcoin-1c304e.netlify.app',
-      deprecated: false,
-      id: 1,
     },
     {
       img: journalsCardImg,
@@ -26,20 +32,7 @@ export default function Projects() {
         'A full-stack CRUD React app that allows users to write and store journal entries. An autocomplete search input is provided to help users search and select for specific journal entries based on keywords.',
       github: 'https://github.com/benjipt/journal-app-frontend',
       githubAPI: 'https://github.com/benjipt/journal-app-api',
-      site: 'https://journals-app-frontend.herokuapp.com',
-      deprecated: true,
-      id: 2,
-    },
-    {
-      img: cryptologCardImg,
-      name: 'Cryptolog',
-      description:
-        'A full-stack CRUD React app that allows users to maintain a log of all cryptocurrency buy/sell transactions on exchanges that they transact on.',
-      github: 'https://github.com/benjipt/cryptolog_frontend',
-      githubAPI: 'https://github.com/benjipt/cryptolog_api',
-      site: 'https://cryptolog-frontend.herokuapp.com/',
-      deprecated: true,
-      id: 3,
+      // site: 'https://journals-app-frontend.herokuapp.com',
     },
     {
       img: simonCardImg,
@@ -48,22 +41,24 @@ export default function Projects() {
         'A digital recreation of Simon, the memory game from Hasbro, with some modern updates. Sounds were created with Garageband.',
       github: 'https://github.com/benjipt/simon-game',
       site: 'https://wonderful-brahmagupta-3ad87a.netlify.app/',
-      deprecated: false,
-      id: 4,
+    },
+    {
+      img: cryptologCardImg,
+      name: 'Cryptolog',
+      description:
+        'A full-stack CRUD React app that allows users to maintain a log of all cryptocurrency buy/sell transactions on exchanges that they transact on.',
+      github: 'https://github.com/benjipt/cryptolog_frontend',
+      githubAPI: 'https://github.com/benjipt/cryptolog_api',
+      // site: 'https://cryptolog-frontend.herokuapp.com/',
+    },
+    {
+      img: microconomyCardImg,
+      name: 'Microconomy',
+      description: 'Privacy focused referral networks. Full-stack CRUD app using Express JS, deployed on Heroku.',
+      github: 'https://github.com/benjipt/microeconomy',
+      // site: 'http://microeconomy.herokuapp.com/',
     },
   ];
-
-  // const archivedProjects = [
-  //     {
-  //         img: microconomyCardImg,
-  //         name: 'Microconomy',
-  //         description: 'Privacy focused referral networks. Full-stack CRUD app using Express JS, deployed on Heroku.',
-  //         github: 'https://github.com/benjipt/microeconomy',
-  //         site: 'http://microeconomy.herokuapp.com/',
-  //         display: false,
-  //         id: null
-  //     }
-  // ]
 
   return (
     <div className='container bottom text-center'>
@@ -74,6 +69,10 @@ export default function Projects() {
       <div className='row justify-content-center ps-3 pe-3 pb-4'>
         <ProjectCard project={projects[2]} />
         <ProjectCard project={projects[3]} />
+      </div>
+      <div className='row justify-content-center ps-3 pe-3 pb-4'>
+        <ProjectCard project={projects[4]} />
+        <ProjectCard project={projects[5]} />
       </div>
     </div>
   );
