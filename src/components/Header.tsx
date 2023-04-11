92211725b63014404bb023ebd23acc24645cc9c1
Benji Thompson <thompson.benji@gmail.com>
2023-04-11 00:06:53 -0400

    feat: make the top navbar sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,55 +6,9 @@ const imgStyle = {
   height: 'auto',
 };
 
-const activeSectionStyle = {
-  backgroundColor: '#8B687F',
-  color: '#EDEBBF',
-};
-
-const inactiveSectionStyle = {
-  backgroundColor: '#EDEBBF',
-  color: '#8B687F',
-};
-
 export default function Header() {
   return (
     <div className='header text-center'>
-      <nav className='navbar navbar-dark bg-custom ps-4'>
-        <form className='justify-content-start'>
-          <NavLink
-            to='/projects'
-            className='btn btn-sm btn-custom me-3'
-            style={({ isActive }) =>
-              isActive ? activeSectionStyle : inactiveSectionStyle
-            }>
-            PROJECTS
-          </NavLink>
-          <NavLink
-            to='/about'
-            className='btn btn-sm btn-custom'
-            style={({ isActive }) =>
-              isActive ? activeSectionStyle : inactiveSectionStyle
-            }>
-            ABOUT
-          </NavLink>
-        </form>
-        <div className='d-flex flex-row-reverse'>
-          <a
-            className='nav-link'
-            href='https://github.com/benjipt'
-            target='_blank'
-            rel='noreferrer'>
-            Github
-          </a>
-          <a
-            className='nav-link'
-            href='https://www.linkedin.com/in/benjithompson/'
-            target='_blank'
-            rel='noreferrer'>
-            Linkedin
-          </a>
-        </div>
-      </nav>
       <h1 className='mt-5'>Benji Thompson</h1>
       <img
         src={headshot}
