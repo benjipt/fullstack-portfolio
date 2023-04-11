01f1e95b1ea5346097e90372703100ca2148a98f
Benji Thompson <thompson.benji@gmail.com>
2023-04-10 20:25:07 -0400

    feat: Change root element ID and update ReactDOM render method
    Update index.html to use 'app' as the ID for the root element
    Replace React and ReactDOM imports with ReactDOMClient in index.tsx
    Use ReactDOMClient.createRoot and root.render to mount App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
@@ -7,12 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 document.body.classList.add('body-style');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('app');
+if (container) {
+  const root = ReactDOMClient.createRoot(container);
+  root.render(<App />);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
